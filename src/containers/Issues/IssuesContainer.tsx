387b41b9247e0f303a1f429bc47af9c57bcf17bc
saem <saemilee01@gmail.com>
2023-08-30 00:18:40 +0900

    feat: issues 타입 지정

--- src/containers/Issues/IssuesContainer.tsx
+++ src/containers/Issues/IssuesContainer.tsx
@@ -1,6 +1,24 @@
+import {useEffect, useState} from 'react';
 import IssueItem from '../../componenets/Issues/IssueItem';
+import * as Fetcher from '../../apis/Issues';
+import * as Type from '../../types/issues';
 
 const IssuesContainer = () => {
+    const [issues, setIssues] = useState<Type.issues[] | []>([]);
+
+    const getIssues = async (page: number) => {
+        try {
+            const res = await Fetcher.getIssues(page);
+            setIssues((prev: Type.issues[]) => [...prev, res.data]);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    useEffect(() => {
+        getIssues(0);
+    }, []);
+
     return (
         <>
             <h1>IssuesContainer</h1>
