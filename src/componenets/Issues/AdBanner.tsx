3bdf731c16119639166131476135bdbb2f3b581e
saem <saemilee01@gmail.com>
2023-08-30 13:38:20 +0900

    design: 광고 배너 스타일링

--- src/componenets/Issues/AdBanner.tsx
+++ src/componenets/Issues/AdBanner.tsx
@@ -1,11 +1,37 @@
+import styled from 'styled-components';
+
 const AdBanner = () => {
     return (
-        <div>
+        <StyledAdBannerContainer>
             <a href='https://www.wanted.co.kr/' target='_blank' rel='noreferrer'>
+                <span className='ad-tag'>ad.</span>
                 <img alt='ad_banner' src='/asset/images/ad_image.png' />
             </a>
-        </div>
+        </StyledAdBannerContainer>
     );
 };
+
+const StyledAdBannerContainer = styled.div`
+    border-bottom: 1px solid gray;
+    cursor: pointer;
+    a {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        position: relative;
+        .ad-tag {
+            padding: 4px;
+            position: absolute;
+            margin-top: 5px;
+            right: 10px;
+            color: gray;
+            border-radius: 4px;
+            background-color: lightgray;
+        }
+        img {
+            width: 100%;
+        }
+    }
+`;
 
 export default AdBanner;
