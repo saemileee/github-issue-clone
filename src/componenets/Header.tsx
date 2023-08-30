f3c0c4b9573ca34e305cebf9df47565e084451ca
saem <saemilee01@gmail.com>
2023-08-30 15:26:24 +0900

    design: 헤더 스타일링

--- src/componenets/Header.tsx
+++ src/componenets/Header.tsx
@@ -1,11 +1,22 @@
+import styled from 'styled-components';
 import {OWNER, REPO} from '../constants/repoInfo';
 
 const Header = () => {
     return (
-        <header>
-            <span>{OWNER}</span>/<span>{REPO}</span>
-        </header>
+        <StyledHeader>
+            <span className='owner'>{OWNER}</span> / <span className='repo'>{REPO}</span>
+        </StyledHeader>
     );
 };
+
+const StyledHeader = styled.header`
+    padding: 16px 0 16px 0;
+    min-width: 100vw;
+    background-color: lightgray;
+    text-align: center;
+    .repo {
+        font-weight: 600;
+    }
+`;
 
 export default Header;
