625aa6d8e316f1a0c996781a2e218483dbb3ae5e
saem <saemilee01@gmail.com>
2023-08-30 22:31:08 +0900

    design: 404 페이지 디자인

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,54 @@
-const NotFound = ({errorStatus}: {errorStatus?: number | string} = {errorStatus: 404}) => {
+import styled from 'styled-components';
+import {DefaultContainer} from '../styles/DefaultContainer.styled';
+import colorPalette from '../styles/colorPalette.styled';
+import {useNavigate} from 'react-router-dom';
+import ROUTES from '../constants/routes';
+
+const NotFound = ({errorStatus}: {errorStatus?: number | string}) => {
+    const navigate = useNavigate();
+
+    const navigateToMain = () => {
+        navigate(ROUTES.ISSUES);
+    };
+
     return (
-        <div>
-            <p>{errorStatus}</p>
-            <p>페이지를 찾을 수 없습니다</p>
-        </div>
+        <DefaultContainer>
+            <StyledNotFoundContainer>
+                <p className='error-status'>{errorStatus ? errorStatus : 404}</p>
+                <p className='message'>페이지를 찾을 수 없습니다 :(</p>
+                <button onClick={navigateToMain}>메인으로 돌아가기</button>
+            </StyledNotFoundContainer>
+        </DefaultContainer>
     );
 };
+
+const StyledNotFoundContainer = styled.div`
+    margin-top: 50px;
+    text-align: center;
+    .error-status {
+        margin: 0;
+        font-size: 70px;
+        font-weight: 900;
+        color: ${colorPalette.highLighting};
+    }
+
+    .message {
+        padding-top: 0px;
+        font-size: 26px;
+        font-weight: 800;
+        color: ${colorPalette.textSubtitle};
+    }
+
+    button {
+        padding: 16px;
+        font-size: 18px;
+        font-weight: 600;
+        color: white;
+        background-color: ${colorPalette.highLighting};
+        border: none;
+        border-radius: 12px;
+        cursor: pointer;
+    }
+`;
 
 export default NotFound;
