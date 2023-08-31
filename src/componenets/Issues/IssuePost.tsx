a65175414996decc2fcfab53ce18c81644882dee
saem <saemilee01@gmail.com>
2023-08-31 21:26:46 +0900

    refactor: 마크다운 라이브러리 수정

--- src/componenets/Issues/IssuePost.tsx
+++ src/componenets/Issues/IssuePost.tsx
@@ -1,4 +1,3 @@
-import {ReactMarkdown} from 'react-markdown/lib/react-markdown';
 import * as Type from '../../types/issues';
 
 import IssueItem from '../../componenets/Issues/IssueItem';
@@ -6,10 +5,7 @@ import IssueItem from '../../componenets/Issues/IssueItem';
 import styled from 'styled-components';
 import colorPalette from '../../styles/colorPalette.styled';
 
-import remarkGfm from 'remark-gfm';
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
-import {materialDark} from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import rehypeRaw from 'rehype-raw';
+import MarkdownPreview from '@uiw/react-markdown-preview';
 
 interface IssuePostProps {
     issueInfo: Type.issuePost;
@@ -27,64 +23,7 @@ const IssuePost = ({issueInfo}: IssuePostProps) => {
                 <IssueItem issue={{number, title, user, created_at, comments}} />
             </div>
             <div className='post-body-container'>
-                <ReactMarkdown
-                    children={body}
-                    remarkPlugins={[remarkGfm]}
-                    disallowedElements={['video']}
-                    components={{
-                        blockquote: ({node, ...props}) => (
-                            <blockquote
-                                style={{
-                                    marginLeft: '12px',
-                                    paddingLeft: '14px',
-                                    borderLeft: `4px solid ${colorPalette.textSubtitle}`,
-                                }}
-                                {...props}
-                            />
-                        ),
-                        p: ({node, ...props}) => <p style={{lineHeight: '1.5rem'}} {...props} />,
-                        li: ({node, ...props}) => (
-                            <li style={{lineHeight: '1.5rem', marginBottom: '12px'}} {...props} />
-                        ),
-                        img: ({node, ...props}) => (
-                            // eslint-disable-next-line jsx-a11y/alt-text
-                            <img
-                                style={{
-                                    width: '100%',
-                                }}
-                                {...props}
-                            />
-                        ),
-                        code({node, inline, className, children, ...props}) {
-                            const match = /language-(\w+)/.exec(className || '');
-                            return !inline ? (
-                                <SyntaxHighlighter
-                                    language={match ? match[1] : 'javascript'}
-                                    PreTag='div'
-                                    {...props}
-                                    style={materialDark}
-                                >
-                                    {String(children).replace(/\n$/, '')}
-                                </SyntaxHighlighter>
-                            ) : (
-                                <code
-                                    style={{
-                                        backgroundColor: colorPalette.textCode,
-                                        padding: '2px 4px 2px 4px',
-                                        borderRadius: '3px',
-                                        fontSize: '14px',
-                                    }}
-                                    {...props}
-                                >
-                                    {children}
-                                </code>
-                            );
-                        },
-                    }}
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-ignore
-                    rehypePlugins={[rehypeRaw]}
-                />
+                <MarkdownPreview source={body} />
             </div>
         </StyledIssuePostContainer>
     );
