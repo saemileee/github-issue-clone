564182ad70c072248360b03775c3885e3dd4807c
saem <saemilee01@gmail.com>
2023-09-04 12:40:54 +0900

    refactor: getIssues 함수 메모이제이션으로 불필요한 함수 인스턴스 재생성 방지

--- src/controllers/useIssues.ts
+++ src/controllers/useIssues.ts
@@ -10,48 +10,51 @@ import {INVALID_ERROR_MSG} from '../constants/messages';
 const useIssues = () => {
     const [issuesState, setIssuesState] = useRecoilState(issuesStateAtom);
 
-    const getIssues = async (page: number) => {
-        try {
-            setIssuesState(prev => ({
-                ...prev,
-                moreData: false,
-                isRefetchNeeded: false,
-            }));
-            const res = await api.getIssues(page);
-            const newIssues = res.data;
-
-            setIssuesState(prev => {
-                // 서버 통신 전 코멘트 정렬이 변경될 경우 기존 배열 필터링하고 새로운 값 받기
-                const filteredIssues = prev.issues.filter(
-                    preIssue =>
-                        !newIssues.some(
-                            (newIssue: Type.issueItem) => newIssue.number === preIssue.number
-                        )
-                );
-                return {
-                    ...prev,
-                    moreData: true,
-                    issues: [...filteredIssues, ...newIssues],
-                };
-            });
-
-            // 마지막 불러온 페이지가 빈 (마지막 페이지 +1 )페이지인 경우 더이상 무한스크롤 안되게 세팅
-            if (!newIssues.length || newIssues.length < PER_PAGE) {
-                return setIssuesState(prev => ({
+    const getIssues = useCallback(
+        async (page: number) => {
+            try {
+                setIssuesState(prev => ({
                     ...prev,
                     moreData: false,
+                    isRefetchNeeded: false,
                 }));
+                const res = await api.getIssues(page);
+                const newIssues = res.data;
+
+                setIssuesState(prev => {
+                    // 서버 통신 전 코멘트 정렬이 변경될 경우 기존 배열 필터링하고 새로운 값 받기
+                    const filteredIssues = prev.issues.filter(
+                        preIssue =>
+                            !newIssues.some(
+                                (newIssue: Type.issueItem) => newIssue.number === preIssue.number
+                            )
+                    );
+                    return {
+                        ...prev,
+                        moreData: true,
+                        issues: [...filteredIssues, ...newIssues],
+                    };
+                });
+
+                // 마지막 불러온 페이지가 빈 (마지막 페이지 +1 )페이지인 경우 더이상 무한스크롤 안되게 세팅
+                if (!newIssues.length || newIssues.length < PER_PAGE) {
+                    return setIssuesState(prev => ({
+                        ...prev,
+                        moreData: false,
+                    }));
+                }
+            } catch (e) {
+                const error = e as AxiosError;
+                setIssuesState(prev => ({
+                    ...prev,
+                    errorStatus: error.response?.status ?? INVALID_ERROR_MSG,
+                }));
+            } finally {
+                setIssuesState(prev => ({...prev, isLoading: false}));
             }
-        } catch (e) {
-            const error = e as AxiosError;
-            setIssuesState(prev => ({
-                ...prev,
-                errorStatus: error.response?.status ?? INVALID_ERROR_MSG,
-            }));
-        } finally {
-            setIssuesState(prev => ({...prev, isLoading: false}));
-        }
-    };
+        },
+        [setIssuesState]
+    );
 
     const getNextPage = () => {
         const newPageCount = issuesState.pageCount + 1;
